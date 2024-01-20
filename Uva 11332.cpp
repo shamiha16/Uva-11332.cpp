1359793c1aa1111a605f9db697ae93fd1b42dfc6
shamiha16 <155057582+shamiha16@users.noreply.github.com>
2024-01-20 23:36:26 +0600

    Update Uva 11332.cpp

--- Uva 11332.cpp
+++ Uva 11332.cpp
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    
+    while (scanf("%d", &n) && n != 0) {
+        while (n >= 10) {
+            int sum = 0;
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
+        }
+        printf("%d\n", n);
+    }
+
+    return 0;
+}
